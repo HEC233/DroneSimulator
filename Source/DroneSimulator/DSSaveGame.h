58c800456a3e7b8418ee70f5b13fdeefb470a9ff
Mossworm <jwideung@naver.com>
2023-05-31 22:53:38 +0900

    프리셋 저장 및 불러오기

--- Source/DroneSimulator/DSSaveGame.h
+++ Source/DroneSimulator/DSSaveGame.h
@@ -35,7 +35,7 @@ public:
 	float CurrentMoveSpeed = 100.0f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SaveData")
-	FString CurrentTargetPreset = "프리셋1";
+	FString CurrentTargetPreset = TEXT("프리셋1");
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SaveData")
 	float CurrentFOV = 60.0f;
