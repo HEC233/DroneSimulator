62ad75a410cd3651809a99f01b06a08c3e47ec51
Ko Jaehyeop <wja04003@naver.com>
2023-07-20 22:02:01 +0900

    타겟 이름 얻어오는 방식 변경

--- Source/DroneSimulator/Components/DSCaptureComponent.cpp
+++ Source/DroneSimulator/Components/DSCaptureComponent.cpp
@@ -47,11 +47,17 @@ void UDSCaptureComponent::TakeScreenShot(int32 CaptureIndex)
 	const FDateTime CurrentTime = FDateTime::Now();
 	const FString TimeString = CurrentTime.ToString(TEXT("%Y.%m.%d-%H.%M.%S"));
 	FString TargetName = CaptureTargetActor->GetActorNameOrLabel();
+	FString TargetAbsoulteName = TargetName;
 
 	FProperty* NameProp = CaptureTargetActor->GetClass()->FindPropertyByName(TEXT("TargetName"));
 	if (NameProp)
 	{
 		NameProp->GetValue_InContainer(CaptureTargetActor, &TargetName);
+	}
+	NameProp = CaptureTargetActor->GetClass()->FindPropertyByName(TEXT("TargetAbsoluteName"));
+	if (NameProp)
+	{
+		NameProp->GetValue_InContainer(CaptureTargetActor, &TargetAbsoulteName);
 	}
 
 	FString ImageFilePath = FPaths::Combine(FPlatformMisc::ProjectDir(), *FString::Printf(TEXT("Captures\\%06d - %s Image - %s.jpg"), CaptureIndex, *TargetName, *TimeString));
@@ -95,13 +101,8 @@ void UDSCaptureComponent::TakeScreenShot(int32 CaptureIndex)
 	int32 MaxX = FMath::CeilToInt(Max.X);
 	int32 MaxY = FMath::CeilToInt(Max.Y);
 
-	int32 StringSharpIdx;
-	if (!TargetName.FindChar(TCHAR('#'), StringSharpIdx))
-	{
-		StringSharpIdx = TargetName.Len();
-	}
 	UE_LOG(LogTemp, Log, TEXT("Min: %s, Max: %s"), *Min.ToString(), *Max.ToString());
-	FString LabelingText = FString::Printf(TEXT("%s,%d,%d,%d,%d"), *TargetName.Mid(0, StringSharpIdx), MinX, MinY, MaxX - MinX, MaxY - MinY);
+	FString LabelingText = FString::Printf(TEXT("%s,%d,%d,%d,%d"), *TargetAbsoulteName, MinX, MinY, MaxX - MinX, MaxY - MinY);
 	FFileHelper::SaveStringToFile(*LabelingText, *TextFilePath);
 
 	if (ImageSavedOK)
